f16a12e48bfb091b487d6d16a6c8ccec02b682cb
Aaron <aaron.broderick@student-cs.fr>
2024-01-29 11:01:10 +0100

    rdy for slurm submit

--- torchtmpl/data.py
+++ torchtmpl/data.py
@@ -38,7 +38,7 @@ def get_dataloaders(data_config, use_cuda):
     #     download=True,
     #     transform=input_transform,
     # )
-    base_dataset = data_set.GeoLifeDataset(data_config["trainpath"])
+    base_dataset = data_set.GeoLifeDataset(data_config["trainpath"], data_portion=data_config["data_portion"])
     logging.info(f"  - I loaded {len(base_dataset)} samples")
 
     indices = list(range(len(base_dataset)))

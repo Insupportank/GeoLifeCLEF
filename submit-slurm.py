dabc9e355abf1e206115eced9a70fd44940fb3b0
Julien MICHEL <julien.michel@student-cs.fr>
2024-02-08 15:08:26 +0100

    resnet34 with longer time limit

--- submit-slurm.py
+++ submit-slurm.py
@@ -12,7 +12,7 @@ def makejob(commit_id, configpath, nruns):
 #SBATCH --job-name=deep_bimodel
 #SBATCH --nodes=1
 #SBATCH --partition=gpu_prod_long
-#SBATCH --time=60:00:00
+#SBATCH --time=50:00:00
 #SBATCH --output=logslurms/slurm-%A_%a.out
 #SBATCH --error=logslurms/slurm-%A_%a.err
 #SBATCH --array=1-{nruns}
